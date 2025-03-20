a432dbbd40e9cad5bc7a764d07dc34209626f57c
Amarnath T <154723453+Amar0th05@users.noreply.github.com>
2025-03-20 22:58:54 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,12 @@ import Tech from './components/Tech'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
 
-<div class="absolute top-0 z-[-2] h-screen w-screen"></div>
 const App = () => {
   
   return (
     <>
-    <div class="fixed -z-10 min-h-screen w-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-    <main className='flex flex-col items-center px-4 md:px-8 lg:px-16'>
+    <div class="fixed -z-10 h-auto w-full min-h-full bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+    <main className='flex flex-col items-center px-4 md:px-8 lg:px-16 w-full min-h-screen'>
       <Navbar/>
       <Hero/>
       <Tech/>
